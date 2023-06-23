9d6de3bb0403d701e0a209d08fc571df86577757
Anthony Berg <smyalygames@gmail.com>
2023-06-23 16:40:22 +0100

    feat(theme): add docs github directory

--- src/theme.config.tsx
+++ src/theme.config.tsx
@@ -1,6 +1,7 @@
 export default {
   logo: <span>Flight Sim on Linux</span>,
   project: {
-    link: 'https://github.com/smyalygames/flightsim-on-linux'
-  }
+    link: 'https://github.com/smyalygames/flightsim-on-linux',
+  },
+  docsRepositoryBase: 'https://github.com/smyalygames/flightsim-on-linux/blob/main',
 }
