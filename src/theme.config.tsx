e9dd78fe873fce172079d8566cca1fdaefb25dfb
Anthony Berg <smyalygames@gmail.com>
2023-06-24 16:31:30 +0100

    refactor: run eslint on all files

--- src/theme.config.tsx
+++ src/theme.config.tsx
@@ -5,12 +5,13 @@ export default {
   project: {
     link: 'https://github.com/smyalygames/flightsim-on-linux',
   },
-  docsRepositoryBase: 'https://github.com/smyalygames/flightsim-on-linux/blob/main',
+  docsRepositoryBase:
+    'https://github.com/smyalygames/flightsim-on-linux/blob/main',
   logo: <span>Flight Sim on Linux</span>,
   useNextSeoProps() {
     return {
-      titleTemplate: '%s - Flight Sim on Linux'
-    }
+      titleTemplate: '%s - Flight Sim on Linux',
+    };
   },
   head: () => {
     // TODO Figure out the reason for these ESLint errors
@@ -18,24 +19,28 @@ export default {
     const { asPath, defaultLocale, locale } = useRouter();
     // eslint-disable-next-line react-hooks/rules-of-hooks
     const { frontMatter } = useConfig();
-    const url = 'https://www.flightsimonlinux.com' + (defaultLocale === locale ? asPath : `/${locale}${asPath}`);
+    const url =
+      'https://www.flightsimonlinux.com' +
+      (defaultLocale === locale ? asPath : `/${locale}${asPath}`);
 
     return (
       <>
         <meta property="og:url" content={url} />
-        <meta property="og:title" content={frontMatter.title || 'Flight Sim on Linux'} />
+        <meta
+          property="og:title"
+          content={frontMatter.title || 'Flight Sim on Linux'}
+        />
         <meta
           property="og:description"
-          content={frontMatter.description || 'Guides to running Flight Simulators on Linux'}
+          content={
+            frontMatter.description ||
+            'Guides to running Flight Simulators on Linux'
+          }
         />
       </>
-    )
+    );
   },
   footer: {
-    text: (
-      <span>
-        © Anthony Berg {new Date().getFullYear()}
-      </span>
-    )
-  }
-}
+    text: <span>© Anthony Berg {new Date().getFullYear()}</span>,
+  },
+};
