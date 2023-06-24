8e0b0c3c035b40de036243c603f0c91ed0423bc3
Anthony Berg <smyalygames@gmail.com>
2023-06-24 02:08:03 +0100

    feat: add meta for each page in theme.config.tsx

--- src/theme.config.tsx
+++ src/theme.config.tsx
@@ -1,3 +1,6 @@
+import { useRouter } from 'next/router';
+import { useConfig } from 'nextra-theme-docs';
+
 export default {
   project: {
     link: 'https://github.com/smyalygames/flightsim-on-linux',
@@ -8,6 +11,22 @@ export default {
     return {
       titleTemplate: '%s - Flight Sim on Linux'
     }
+  },
+  head: () => {
+    const { asPath, defaultLocale, locale } = useRouter();
+    const { frontMatter } = useConfig();
+    const url = 'https://www.flightsimonlinux.com' + (defaultLocale === locale ? asPath : `/${locale}${asPath}`);
+
+    return (
+      <>
+        <meta property="og:url" content={url} />
+        <meta property="og:title" content={frontMatter.title || 'Flight Sim on Linux'} />
+        <meta
+          property="og:description"
+          content={frontMatter.description || 'Guides to running Flight Simulators on Linux'}
+        />
+      </>
+    )
   },
   footer: {
     text: (
