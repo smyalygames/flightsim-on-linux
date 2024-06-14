be4eb87de34db60a4e1f6b809182f32a7ed54985
Anthony <smyalygames@gmail.com>
2024-06-15 00:09:31 +0200

    refactor: change URL casing and add redirects for old URLs

--- next.config.js
+++ next.config.js
@@ -1,7 +1,22 @@
 /** @type {import('next').NextConfig} */
 const withNextra = require('nextra')({
     theme: 'nextra-theme-docs',
-    themeConfig: './src/theme.config.tsx'
+    themeConfig: './src/theme.config.tsx',
 })
 
-module.exports = withNextra()
+module.exports = withNextra({
+    async redirects() {
+        return [
+            {
+                source: '/msfs/utils/navigraphHub',
+                destination: '/msfs/utils/navigraph-hub',
+                permanent: true,
+            },
+            {
+                source: '/msfs/utils/navigraphSimlink',
+                destination: '/msfs/utils/navigraph-simlink',
+                permanent: true,
+            }
+        ]
+    }
+})
