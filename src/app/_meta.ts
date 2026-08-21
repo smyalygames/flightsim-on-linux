a21400d3677ca536f7e789fdca8392399321a6b6
Anthony Berg <smyalygames@gmail.com>
2026-08-21 10:28:14 +0200

    fix(meta): remove manual title overrides in meta

--- src/app/_meta.ts
+++ src/app/_meta.ts
@@ -3,21 +3,14 @@ import type { MetaRecord } from 'nextra';
 const meta: MetaRecord = {
   index: {
     title: 'Home',
-    type: 'page',
   },
   about: {
     title: 'About',
     type: 'page',
   },
-  msfs: {
-    title: "Microsoft Flight Simulator 2020"
-  },
-  xp11: {
-    title: "X-Plane 11"
-  },
-  xp12: {
-    title: "X-Plane 2"
-  }
+  msfs: '',
+  xp11: '',
+  xp12: ''
 };
 
 export default meta;
